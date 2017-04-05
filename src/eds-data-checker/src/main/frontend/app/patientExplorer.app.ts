d29f62059d101da7dc8dc2b10bf6a3ee3fa231c5
ergosoftuk <qPellHGfNE4FpAnd0D0a>
2017-04-05 14:48:46 +0100

    Centralized Application class

--- src/eds-data-checker/src/main/frontend/app/patientExplorer.app.ts
+++ src/eds-data-checker/src/main/frontend/app/patientExplorer.app.ts
@@ -1,6 +1,9 @@
+// Sytling
+import '../content/css/index.css';
+import '../content/less/index.less';
+
 // Core
 import {NgModule} from '@angular/core';
-import {Application} from "./application";
 
 // Modules
 import {RecordViewerModule} from "./recordViewer/recordViewer.module";
@@ -11,6 +14,8 @@ import {RecordViewerComponent} from "./recordViewer/recordViewer.component";
 import {CountReportComponent} from "./countReport/countReport.component";
 import {SqlEditorModule} from "./sqlEditor/sqlEditor.module";
 import {SqlEditorComponent} from "./sqlEditor/sqlEditor.component";
+import {PatientExplorerMenuService} from "./patientExplorer.menu";
+import {Application} from "eds-common-js";
 
 @NgModule(
 	Application.Define({
@@ -24,7 +29,8 @@ import {SqlEditorComponent} from "./sqlEditor/sqlEditor.component";
 			{name: 'app.countReports', url: '/countReports', component : CountReportComponent },
 			{name: 'app.sqlEditor', url: '/sqlEditor', component : SqlEditorComponent }
 		],
-		defaultState : { state: 'app.recordViewer', params: {} }
+		defaultState : { state: 'app.recordViewer', params: {} },
+		menuManager : PatientExplorerMenuService
 	})
 )
 export class AppModule {}
